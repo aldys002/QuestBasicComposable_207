af038f9c7129d3d66c45d6c2fcf39376ade1732d
aldys002 <igidiaaldys@gmail.com>
2025-10-16 14:11:41 +0700

    menambah gambar

--- app/src/main/java/com/example/layout/Tataletak.kt
+++ app/src/main/java/com/example/layout/Tataletak.kt
@@ -90,7 +90,7 @@ fun TataLetakRowColumn(modifier: Modifier) {
 
 @Composable
 fun TataletakBoxColumnRow(modifier: Modifier) {
-    val gambar = painterResource(id = R.drawable.notasibalok)
+    val gambar = painterResource(id = R.drawable.ic_launcher_foreground)
     Column {
         Box(
             modifier = modifier
