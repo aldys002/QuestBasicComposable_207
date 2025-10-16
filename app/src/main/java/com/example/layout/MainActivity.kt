9eb1c932b2e7ea2c420be9f6131cd97fb1d81581
aldys002 <igidiaaldys@gmail.com>
2025-10-16 13:34:27 +0700

    menambahkan struktur Column pada TataletakBoxColumnRow

--- app/src/main/java/com/example/layout/MainActivity.kt
+++ app/src/main/java/com/example/layout/MainActivity.kt
@@ -10,19 +10,19 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.layout.ui.theme.LayoutTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            LayoutTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+            MyLayoutTheme {
+                Scaffold (modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    //Panggil composable layout utama dengan oaddung dari scaffold
+                    TataletakColumnRow(
+                        modifier = Modifier.padding
                     )
                 }
             }
