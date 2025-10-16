02bd723094e3ffba475a2fdbe014e76e41ee0cbb
aldys002 <igidiaaldys@gmail.com>
2025-10-16 12:55:07 +0700

    feat: add Text components inside TataletakRow

--- app/src/main/java/com/example/layout/Tataletak.kt
+++ app/src/main/java/com/example/layout/Tataletak.kt
@@ -45,8 +45,13 @@ fun TataletakColumnRow(modifier: Modifier) {
         //Baris1
         Row(modifier = modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceEvenly) {
+            Text(text = "Komponen1Baris1")
+            Text(text = "Komponen2Baris1")
+            Text(text = "Komponen3Baris1")
         }
+
     }
 }
 
+@Composable
 
