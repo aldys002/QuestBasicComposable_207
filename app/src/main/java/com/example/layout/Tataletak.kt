d9a53b1f14ed41f82c0e5f3161b4e2d7b6195373
aldys002 <igidiaaldys@gmail.com>
2025-10-16 12:52:46 +0700

    feat: add TataletakColumnRow composable function

--- app/src/main/java/com/example/layout/Tataletak.kt
+++ app/src/main/java/com/example/layout/Tataletak.kt
@@ -1,7 +1,9 @@
 package com.example.layout
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -36,3 +38,15 @@ fun TataletakRow(modifier: Modifier){
         Text(text = "Column 2")
     }
 }
+
+@Composable
+fun TataletakColumnRow(modifier: Modifier) {
+    Column() {
+        //Baris1
+        Row(modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+        }
+    }
+}
+
+
