c32b3071882ef088059573200c536979b9369330
aldys002 <igidiaaldys@gmail.com>
2025-10-16 13:52:54 +0700

    menghapus import yang tidak di gunakan

--- app/src/main/java/com/example/layout/Tataletak.kt
+++ app/src/main/java/com/example/layout/Tataletak.kt
@@ -1,6 +1,6 @@
 package com.example.layout
 
-import android.graphics.Paint
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -16,7 +16,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
